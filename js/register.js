4f6f6fe0a7d296a3d4158dd51ce8b8c0b5cbf377
Melike ISIK <melike_gs_8-@hotmail.com>
2023-11-08 14:22:04 +0300

    Parola görünürlüğü ayarlanmıştır

--- js/register.js
+++ js/register.js
@@ -7,8 +7,11 @@ const checkbox3 = document.getElementById("cbx-43");
 const gender = document.querySelectorAll(".gender-button button")
 const submitBtn = document.querySelector(".last-button")
 const errorBox = document.querySelector(".error-box")
+const passwordVisibility = document.querySelector(".eye-icon")
+//const openEye=document.getElementById("open-eye")
+const closeEye=document.getElementById("close-eye")
 
-
+console.log(passwordVisibility)
 let userList = JSON.parse(localStorage.getItem("userList")) || []
 //localStorage.removeItem("userList")
 
@@ -51,14 +54,36 @@ function setUser(){
 
     const mailAddres = userList.find(item => item.userMail == email.value)
         if(mailAddres){
-            console.log("kullanici var")
+            errorBox.style.display="flex"
+            let pTag = errorBox.querySelectorAll("p")
+            if(pTag.length==0){
+                let errorMessage= document.createElement("p")
+                errorMessage.innerHTML= "Bu maile ait kullanıcı bulunmaktadır. Yeni mail deneyiniz."
+                email.setAttribute("style", "background-color:#fff4f6;border: 1px solid #d0021b;")
+                errorBox.appendChild(errorMessage)  
+            }else{
+                "style", "background-color:#fff4f6;border: 1px solid #d0021b;"
+                pTag[0].innerHTML = "Bu maile ait kullanıcı bulunmaktadır. Yeni mail deneyiniz."
+            }
+            
         }else{
             userList.push(user)
             localStorage.setItem("userList" ,JSON.stringify(userList))
         }
 }
 
-
+passwordVisibility.addEventListener("click", e=>{
+    console.log(closeEye.classList)
+    if(password.type=="password"){
+        password.type="text"
+        closeEye.classList.remove("fa-solid","fa-eye-slash")
+        closeEye.classList.add("fa-solid","fa-eye")
+    }else{
+        password.type="password"
+        closeEye.classList.remove("fa-solid","fa-eye")
+        closeEye.classList.add("fa-solid","fa-eye-slash")
+    }
+})
 submitBtn.addEventListener("click" ,e=>{
     e.preventDefault();
     [...form.elements].forEach(e=> {
