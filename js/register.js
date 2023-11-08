30b8731d2295f653ab8a4d96167ad715c1c0f746
Melike ISIK <melike_gs_8-@hotmail.com>
2023-11-08 12:08:27 +0300

    Uye olma işlemleri eklendi.

---
+++ js/register.js
@@ -0,0 +1,92 @@
+const form = document.getElementById("form")
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const checkbox1 = document.getElementById("example-1");
+const checkbox2 = document.getElementById("example-2");
+const checkbox3 = document.getElementById("cbx-43");
+const gender = document.querySelectorAll(".gender-button button")
+const submitBtn = document.querySelector(".last-button")
+const errorBox = document.querySelector(".error-box")
+
+
+let userList = JSON.parse(localStorage.getItem("userList")) || []
+//localStorage.removeItem("userList")
+
+
+function msg (eleman){
+    if(eleman.validity.typeMismatch) {
+        return "Lütfen mailinizi adresinizi kontrol edin"      
+    }else if (eleman.validity.patternMismatch) {
+        return "Şifreniz en az 1 harf ve en az 1 rakam içermelidir." 
+    }else if (eleman.validity.valueMissing) {
+        return "Lütfen bilgileri eksiksiz doldurunuz" 
+    }else if (eleman.validity.tooLong) {
+        return "Şifreniz en fazla 64 karakter olmalıdır" 
+    } else if (eleman.validity.tooShort) {
+        return "Şifreniz en az 7 karakter olmalıdır" 
+    }else if (eleman.validity.patternMismatch) {
+        return "Şifreniz en az 1 rakam ve en az 1 harf içermelidir" 
+    }
+}
+function checkValidity(eleman){
+    errorBox.style.display="flex"
+    let pTag = errorBox.querySelectorAll("p")
+    if(pTag.length==0){
+        let errorMessage= document.createElement("p")
+        errorMessage.innerHTML= msg(eleman)
+        errorBox.appendChild(errorMessage)  
+    }else{
+        pTag[0].innerHTML = msg(eleman)
+    }}
+
+function setUser(){
+    const userMail = email.value;
+    const userPassword = password.value;
+    console.log(userMail, userPassword)
+
+    const user ={
+        userMail : userMail,
+        userPassword : userPassword,
+    }
+
+    const mailAddres = userList.find(item => item.userMail == email.value)
+        if(mailAddres){
+            console.log("kullanici var")
+        }else{
+            userList.push(user)
+            localStorage.setItem("userList" ,JSON.stringify(userList))
+        }
+}
+
+
+submitBtn.addEventListener("click" ,e=>{
+    e.preventDefault();
+    [...form.elements].forEach(e=> {
+        if(!e.checkValidity()){
+            e.setAttribute("style", "background-color:#fff4f6;border: 1px solid #d0021b;")
+            checkValidity(e)
+        }else{
+            e.removeAttribute("style")
+            setUser();
+        }
+    })
+
+})
+
+
+var userGender;
+gender.forEach(button => button.addEventListener("click", e=>{
+    e.preventDefault()
+    gender.forEach(genderBtn => genderBtn.classList.remove("genderClass"))
+    //console.log(e.target.id)
+    e.target.classList.add("genderClass")
+    /*
+    if(e.target.id == 1){
+        userGender = "Kadin"
+    }else{
+        userGender = "Erkek"
+        }
+    }*/
+}
+))
+
