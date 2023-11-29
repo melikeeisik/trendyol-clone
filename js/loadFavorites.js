e284a9799e1651e387c45fa834fa4b77eaf3f34f
Melike ISIK <melike_gs_8-@hotmail.com>
2023-11-29 15:25:48 +0300

    Ürün detay sayfasında bulunan zoomlama özelliği parametrik hale getirildi

--- js/loadFavorites.js
+++ js/loadFavorites.js
@@ -6,6 +6,10 @@ function loadFav(){
             productTilte:"Avon",
             productName:"Wish Of Love Kadın Parfüm Edt 50 Ml. 1193492",
             productPrice : 300,
+            otherProductImg : [
+                "./images/avon1.webp","./images/avon2.webp"
+            ]
+           
         },
         {
             productId:2,
@@ -13,6 +17,10 @@ function loadFav(){
             productTilte:"TRENDYOLMİLLA ",
             productName:"Siyah Straight Yüksek Bel Nervür Dikişli Dokuma Pantolon TWOSS21PL0093",
             productPrice : 150,
+            otherProductImg : [
+                "https://cdn.dsmcdn.com/mnresize/1200/1800/ty948/product/media/images/20230612/13/385007856/123408601/7/7_org_zoom.jpg",
+                "https://cdn.dsmcdn.com/ty950/product/media/images/20230612/13/385007856/123408601/6/6_org_zoom.jpg"
+            ]
         },
         {
             productId:3,
@@ -20,6 +28,10 @@ function loadFav(){
             productTilte:"Bargello",
             productName:"122 Oriental Edp 50 Ml Kadın Parfüm 8691841304622 BRGL122",
             productPrice : 250,
+            otherProductImg : [
+                "https://cdn.dsmcdn.com/ty983/product/media/images/20230808/20/401324008/113997666/3/3_org_zoom.jpg",
+            ]
+            
         },
         {
             productId:4,
@@ -27,6 +39,7 @@ function loadFav(){
             productTilte:"The Purest Solutions",
             productName:"Leke Karşıtı Arbutin Cilt Bakım Serumu 30 Ml (arbutin %2 + Hyaluronic Acid) TPS103",
             productPrice : 100,
+            otherProductImg:[]
         },
         {
             productId:5,
@@ -34,6 +47,12 @@ function loadFav(){
             productTilte:"DR TAYYARÖZ",
             productName:"Cilt Beyazlatıcı Leke Kremi 30+Spf DRTAYYARÖZBLEMİSHCREAM",
             productPrice : 50,
+            otherProductImg : [
+                "https://cdn.dsmcdn.com/ty1029/product/media/images/prod/SPM/PIM/20231102/23/0dd7df66-46ce-30d8-8bda-c6c249abb89d/1_org_zoom.jpg",
+                "https://cdn.dsmcdn.com/ty1029/product/media/images/prod/SPM/PIM/20231102/23/7c9237dc-ecf8-3e59-9477-c00afc6aa555/1_org_zoom.jpg"
+            ]
+            
+            
         },
         {
             productId:6,
@@ -41,6 +60,11 @@ function loadFav(){
             productTilte:"The Purest Solutions",
             productName:"Canlandırıcı Ve Gözenek Sıkılaştırıcı Tüm Ciltler Için Glikolik Asit Tonik 200 Ml TPS104",
             productPrice : 200,
+            otherProductImg : [
+                "https://cdn.dsmcdn.com/ty956/product/media/images/20230623/11/388488512/78225778/3/3_org_zoom.jpg",
+                "https://cdn.dsmcdn.com/ty954/product/media/images/20230623/11/388488512/78225778/2/2_org_zoom.jpg",
+                "https://cdn.dsmcdn.com/ty954/product/media/images/20230623/11/388488512/78225778/5/5_org_zoom.jpg"
+            ]
         },
     ]
     let productsAll = JSON.parse(localStorage.getItem("productsAll")) || []
--- js/productDetail.js
+++ js/productDetail.js
@@ -10,11 +10,104 @@ const userContainer = document.querySelector(".user-container")
 const userMailBox= document.querySelector(".user-mail-box")
 const liText = document.getElementById("li-text")
 const countCart = document.querySelector(".count") 
-
+const otherPicContainer = document.querySelector(".other-product-pictures")
+const result = document.querySelector("#myresult")
 let userList = JSON.parse(localStorage.getItem("userList")) || []
 let activeUser = JSON.parse(sessionStorage.getItem("currentloggedin")) || []
 let productsAll = JSON.parse(localStorage.getItem("productsAll")) || [];
 
+function otherPicture(){
+  let otherImg= productsAll.find(item => item.productId == bodyId)
+  if(otherImg.otherProductImg.length == 0){
+    otherPicContainer.setAttribute("style", "display:none")
+  }else{
+    otherPicContainer.removeAttribute("style")
+    console.log(otherImg.otherProductImg.length)
+    for (let i = -1; i< otherImg.otherProductImg.length ; i++){
+      let otherPicBox = document.createElement("div")
+      otherPicBox.classList.add("other-picture")
+      otherPicContainer.append(otherPicBox)
+
+      let otherPic =  document.createElement("img")
+      otherPic.classList.add("other-picture-img")
+      
+      if(i == -1){
+        otherPic.src = otherImg.productImg
+      }else{
+        otherPic.src = otherImg.otherProductImg[i]
+      }
+      otherPicBox.append(otherPic)
+      createClickBox()
+    }
+  }
+}
+function createClickBox(){
+  const btn = document.querySelectorAll(".other-picture-img")
+  btn.forEach(item => {
+    item.addEventListener("click", e => {
+        console.log(item)
+        const myImage = document.getElementById("myimage")
+        myImage.src = item.src;
+        let result = document.getElementById("myresult");
+        result.style.backgroundImage = "url('" + myImage.src + "')";
+    });
+  });
+}
+function imageZoom(imgID, resultID) {
+  var img, lens, result, cx, cy;
+  img = document.getElementById(imgID);
+  result = document.getElementById(resultID);
+  /*create lens:*/
+  lens = document.createElement("div");
+  lens.setAttribute("class", "img-zoom-lens");
+  /*insert lens:*/
+  img.parentElement.insertBefore(lens, img);
+  /*calculate the ratio between result DIV and lens:*/
+  cx = 2.51;
+  cy = 3.285;
+  /*set background properties for the result DIV:*/
+  result.style.backgroundImage = "url('" + img.src + "')";
+  result.style.backgroundSize = (img.width * cx) + "px " + (img.height * cy) + "px";
+  /*execute a function when someone moves the cursor over the image, or the lens:*/
+  lens.addEventListener("mousemove", moveLens);
+  img.addEventListener("mousemove", moveLens);
+  /*and also for touch screens:*/
+  lens.addEventListener("touchmove", moveLens);
+  img.addEventListener("touchmove", moveLens);
+  function moveLens(e) {
+    var pos, x, y;
+    /*prevent any other actions that may occur when moving over the image:*/
+    e.preventDefault();
+    /*get the cursor's x and y positions:*/
+    pos = getCursorPos(e);
+    /*calculate the position of the lens:*/
+    x = pos.x - (lens.offsetWidth / 2);
+    y = pos.y - (lens.offsetHeight / 2);
+    /*prevent the lens from being positioned outside the image:*/
+    if (x > img.width - lens.offsetWidth) {x = img.width - lens.offsetWidth;}
+    if (x < 0) {x = 0;}
+    if (y > img.height - lens.offsetHeight) {y = img.height - lens.offsetHeight;}
+    if (y < 0) {y = 0;}
+    /*set the position of the lens:*/
+    lens.style.left = x + "px";
+    lens.style.top = y + "px";
+    /*display what the lens "sees":*/
+    result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
+  }
+  function getCursorPos(e) {
+    var a, x = 0, y = 0;
+    e = e || window.event;
+    /*get the x and y positions of the image:*/
+    a = img.getBoundingClientRect();
+    /*calculate the cursor's x and y coordinates, relative to the image:*/
+    x = e.pageX - a.left;
+    y = e.pageY - a.top;
+    /*consider any page scrolling:*/
+    x = x - window.pageXOffset;
+    y = y - window.pageYOffset;
+    return {x : x, y : y};
+  }
+}
 
 function control(){
   const user = userList.find(item => item.userMail == activeUser)
@@ -139,10 +232,16 @@ function cartCount(){
 function showProductDetail(productId){
   let product = productsAll.find(item => item.productId == productId)
   bodyId = product.productId
-  document.getElementById("myimage").src = product.productImg
   document.getElementById('brands').textContent = product.productTilte;
   document.getElementById('fullname').textContent = product.productName;
   document.querySelector(".product-price").innerHTML = product.productPrice + " TL"
+  const zoomImg = document.createElement("img")
+  zoomImg.id="myimage"
+  zoomImg.classList.add("product-img-size")
+  zoomImg.src= product.productImg
+  console.log(result)
+  result.before(zoomImg)
+  imageZoom("myimage", "myresult")
 }
 
 userBtn.addEventListener('mouseout', () => {
@@ -165,6 +264,7 @@ function loadPage() {
       addFavorites.querySelector("i").setAttribute("style","color:#ffa500;")
     }
     control()
+    otherPicture()
     cartCount()
   }
 }
