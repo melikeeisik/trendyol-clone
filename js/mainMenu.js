a3a824128251d4bbf92eaff1e52d8082592e186e
Melike ISIK <melike_gs_8-@hotmail.com>
2023-11-30 19:16:02 +0300

    Anasayfadaki ürünler localden çekildi. Ürünlere scroll özelliği eklendi

--- js/mainMenu.js
+++ js/mainMenu.js
@@ -18,6 +18,12 @@ const liText = document.getElementById("li-text")
 const countCart = document.querySelector(".count") 
 const productBoxsImg = document.querySelectorAll(".product-box img")
 const productBoxsDetail = document.querySelectorAll(".product-container")
+const productGrid = document.querySelector(".product-grid")
+const productBox = document.querySelector(".product-box")
+const productAll= JSON.parse(localStorage.getItem("productsAll"))
+const productLeft = document.querySelector(".product-scroll-left")
+const productRight = document.querySelector(".product-scroll-right")
+
 let activeUser = JSON.parse(sessionStorage.getItem("currentloggedin")) || []
 const userList = JSON.parse(localStorage.getItem("userList")) || []
 
@@ -178,45 +184,86 @@ function createUrl(id){
         console.log(newUrl)
     }
 }
-productBoxsImg.forEach(item =>{
-    item.addEventListener("click", e=>{
+
+
+function showProductBox(products, productBox){
+
+    const user = userList.find(item => item.userMail == activeUser)
+
+    const productImg = productBox.querySelector(".product-img-container img")
+    const productBrand = productBox.querySelector(".brands-name")
+    const productName = productBox.querySelector(".full-name")
+    const productPrice = productBox.querySelector(".product-discount-price")
+    const productDetail = productBox.querySelector(".product-detail")
+    const addFavoritesBtn = productBox.querySelectorAll(".product-icon-contanier ")
+    const iTags = productBox.querySelectorAll(".product-icon-contanier i ")
+
+    productBox.setAttribute('style', 'display:flex')
+
+    productBox.id = products.productId
+    productImg.src=products.productImg;
+    productBrand.innerHTML = products.productTilte 
+    productName.innerHTML= products.productName 
+    productPrice.innerHTML=products.productPrice+" TL"
+    
+    productImg.addEventListener("click", e=>{
         createUrl(e.target.closest(".product-box").id)
     })
-})
-productBoxsDetail.forEach(item => {
-    item.addEventListener("click", e=>{
+    productDetail.addEventListener("click", e=>{
         createUrl(e.target.closest(".product-box").id)
     })
+    addFavoritesBtn.forEach(item=>
+        item.addEventListener("click", e=>{
+            e.preventDefault()
+            const productId = e.target.closest(".product-box").id
+            let isFavorites = addFavorites(productId)
+            if(isFavorites){
+                item.querySelector("i").classList.remove("fa-regular","fa-heart")
+                item.querySelector("i").classList.add("fa-solid", "fa-heart")
+                item.setAttribute("style", "color:#ffa500")
+            }else{
+                item.querySelector("i").classList.remove("fa-solid","fa-heart")
+                item.querySelector("i").classList.add("fa-regular","fa-heart")
+                item.removeAttribute("style", "color:#ffa500")
+            }
+        } )
+    )
+    iTags.forEach(iTag => {
+        user.userFavorites.forEach(fav =>{ 
+            if(fav == iTag.closest(".product-box").id){
+                iTag.classList.remove("fa-regular","fa-heart")
+                iTag.classList.add("fa-solid", "fa-heart")
+                iTag.setAttribute("style", "color:#ffa500")
+            }
+        })
+    })  
+}
+productGrid.addEventListener("scroll", (e) => {
+    if (e.target.scrollLeft >= 440) {
+        productRight.style.display="none"
+    } else if (e.target.scrollLeft == 0) {
+        productLeft.style.display="none"
+    } else {
+        productRight.style.display="flex"
+        productLeft.style.display="flex"
+    }
+});
+productLeft.addEventListener("click", e=>{
+    productGrid.scrollLeft -=400
 })
-addFavoritesBtn.forEach(item=>
-    item.addEventListener("click", e=>{
-        e.preventDefault()
-        const productId = e.target.closest(".product-box").id
-        let isFavorites = addFavorites(productId)
-        if(isFavorites){
-            item.querySelector("i").classList.remove("fa-regular","fa-heart")
-            item.querySelector("i").classList.add("fa-solid", "fa-heart")
-            item.setAttribute("style", "color:#ffa500")
-        }else{
-            item.querySelector("i").classList.remove("fa-solid","fa-heart")
-            item.querySelector("i").classList.add("fa-regular","fa-heart")
-            item.removeAttribute("style", "color:#ffa500")
-        }
-    } )
-)
+productRight.addEventListener("click", e=>{
+    productGrid.scrollLeft +=400
+})
 function loadPage(){
+    productAll.forEach(item =>{
+        let newProductBox = productBox.cloneNode(true)                      
+        showProductBox(item, newProductBox)
+        productGrid.append(newProductBox)
+    })
+    productBox.setAttribute("style","display:none")
     const user = userList.find(item => item.userMail == activeUser)
     if(user){
         liText.textContent="Hesabım"
-        iTags.forEach(iTag => {
-            user.userFavorites.forEach(fav =>{ 
-                if(fav == iTag.closest(".product-box").id){
-                    iTag.classList.remove("fa-regular","fa-heart")
-                    iTag.classList.add("fa-solid", "fa-heart")
-                    iTag.setAttribute("style", "color:#ffa500")
-                }
-            })
-        })
         cartCount();
     }
            
